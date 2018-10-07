1b456a7a2a0aa8fb8eb96b1427da0071c8ca9c5b
Joe Bates <joebates@jbimagine.com>
2018-10-07 15:54:31 -0700

    Add in markers and name of locations to google maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import '../App.css';
-
+import axios from 'axios'
 
 class Map extends Component {
 
+  state = {
+    venues:[]
+  }
+
   componentDidMount(){
-    this.renderMap()
+    this.getVenues();
   }
 
   renderMap = () => {
@@ -13,10 +17,32 @@ class Map extends Component {
     window.initMap = this.initMap;
   }
 
+  getVenues = () => {
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
+    const parameters = {
+      client_id: 'TWHL2R1LFV2US323VBSFT31HWQGVYFR4ASEDJXG3TGC5NTFM',
+      client_secret: 'J34A3A4OEOROQ40CPNYZTUA1UPJ45ITU25TEMGJIJ4PQHJAV',
+      query: 'food',
+      near: 'San Francisco',
+      v: '20180710'
+    }
+    axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+      this.setState({
+        venues:response.data.response.groups[0].items}, this.renderMap())
+      console.log(response.data.response.groups[0].items)
+    })
+    .catch(error => {
+      console.log('ERROR!! ' + error)
+    }) 
+  }
+
+  
+
   initMap = () => {
-    new window.google.maps.Map(document.getElementById('map'), {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 37.761640, lng: -122.483890},
-      zoom: 15,
+      zoom: 14,
       mapTypeControl: true,
           mapTypeControlOptions: {
               style: window.google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
@@ -33,6 +59,30 @@ class Map extends Component {
           },
           fullscreenControl: false
     });
+
+    // Create an info window on the google map
+    let infowindow = new window.google.maps.InfoWindow();
+
+
+    this.state.venues.map(myVenue => {
+
+      let contentString = `${myVenue.venue.name}`;
+
+      // Create a marker on the google map
+      let marker = new window.google.maps.Marker({
+        position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
+        map: map,
+        title: myVenue.venue.name
+      });
+
+    marker.addListener('click', function() {
+
+      infowindow.setContent(contentString);
+
+      infowindow.open(map, marker);
+  });
+
+    })    
   }
 
   render() {
