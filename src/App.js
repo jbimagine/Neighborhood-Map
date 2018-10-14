7d79e1b99a71b4318cea82cd21c73b80bd3ed830
Joe Bates <joebates@jbimagine.com>
2018-10-13 20:27:42 -0700

    EP-0001 update to use fetch and react google   map

--- src/App.js
+++ src/App.js
@@ -1,17 +1,65 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation'
 import Map from './components/Map'
+import NeighborhoodApi from './components/NeighborhoodApi'
 import './App.css';
 
 
 class App extends Component {
   state = {
     isMenuVisible: false,
+    venues:[],
+    markers:[],
+    center:[],
+    zoom:12
   }
 
   componentDidMount (){
     document.body.classList.add('map-body')
+
+    NeighborhoodApi.search({
+      near: 'San Francisco, CA',
+      query: 'food'
+    }).then(results => {
+      const { venues } = results.response;
+      const { center } = results.response.geocode.feature.geometry;
+      const markers = venues.map(venue => {
+        return {
+          lat:venue.location.lat,
+          lng:venue.location.lng,
+          isOpen:false,
+          isVisible:true,
+          id: venue.id
+        }
+      });
+      this.setState({ venues, center, markers })
+      console.log(results);
+    })
   }
+
+  closeAllMarkers = () => {
+    const markers = this.state.markers.map(marker => {
+      marker.isOpen = false;
+      return marker;
+    });
+    this.setState( {markers: Object.assign(this.state.markers, markers)} )
+  }
+
+  handleMarkerClick = marker => {
+    this.closeAllMarkers();
+    marker.isOpen = true;
+    this.setState( {markers: Object.assign(this.state.markers, marker)} )
+
+    const venue = this.state.venues.find(venue => venue.id = marker.id)
+  
+    NeighborhoodApi.getVenueDetails(marker.id).then(res => {
+      const newVenue = Object.assign(venue, res.response.venue);
+      this.setState({venues:Object.assign(this.state.venues, newVenue)})
+      console.log(newVenue);
+
+    })
+  
+  };
 
   componentWillUnmount (){
     document.body.classList.remove('map-body')
@@ -28,7 +76,10 @@ class App extends Component {
       isMenuVisible = {this.state.isMenuVisible}
       handleMenuVisibility = {this.handleMenuVisibility}
       />
-      <Map/>
+      <Map
+      {...this.state}
+      handleMarkerClick = {this.handleMarkerClick}
+      />
       </div>
 
     );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,106 +1,66 @@
 import React, { Component } from 'react';
 import '../App.css';
-import axios from 'axios'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
+
+const MyMapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+    defaultZoom = {14}
+    zoom = {props.zoom}
+    defaultCenter={{ lat: 37.761640, lng: -122.483890 }}
+    center = { props.center }
+    >
+    { props.markers &&
+      props.markers.filter(marker => marker.isVisible).map(( marker, idx ) => {
+        const venueInfo = props.venues.find(venue =>  venue.id = marker.id);
+        return (
+         <Marker 
+            key={idx} 
+            position= {{ lat: marker.lat, lng: marker.lng }} 
+            onClick = {() => props.handleMarkerClick(marker) }
+            >
+          {marker.isOpen && 
+            venueInfo.bestPhoto && (
+            <InfoWindow>
+              <React.Fragment>
+                <div style = {{display: 'flex', flexDirerction: 'column', paddingTop: '20px', paddingLeft: '25px'}}>
+                <img
+                  src = {`${venueInfo.bestPhoto.prefix}200x200${
+                    venueInfo.bestPhoto.suffix
+                    }`}
+                     alt ={'Venue'}
+                     />
+                </div>
+              <p style = {{ display:'flex', justifyContent:'center' }}>{venueInfo.name}</p>
+              </React.Fragment>
+            </InfoWindow>
+          )}
+        </Marker>
+      );
+      })}
+
+    </GoogleMap>
+  ))
+)
+
+
 
 class Map extends Component {
-
-  state = {
-    venues:[]
-  }
-
-  componentDidMount(){
-    this.getVenues();
-  }
-
-  renderMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDiucAl8uSDKELoUW25LTlq7xU7DsGG2hY&callback=initMap")
-    window.initMap = this.initMap;
-  }
-
-  getVenues = () => {
-    const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
-    const parameters = {
-      client_id: 'TWHL2R1LFV2US323VBSFT31HWQGVYFR4ASEDJXG3TGC5NTFM',
-      client_secret: 'J34A3A4OEOROQ40CPNYZTUA1UPJ45ITU25TEMGJIJ4PQHJAV',
-      query: 'food',
-      near: 'San Francisco',
-      v: '20180710'
-    }
-    axios.get(endPoint + new URLSearchParams(parameters))
-    .then(response => {
-      this.setState({
-        venues:response.data.response.groups[0].items}, this.renderMap())
-      console.log(response.data.response.groups[0].items)
-    })
-    .catch(error => {
-      console.log('ERROR!! ' + error)
-    }) 
-  }
-
-  
-
-  initMap = () => {
-    let map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 37.761640, lng: -122.483890},
-      zoom: 14,
-      mapTypeControl: true,
-          mapTypeControlOptions: {
-              style: window.google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-              position: window.google.maps.ControlPosition.BOTTOM_CENTER
-          },
-          zoomControl: true,
-          zoomControlOptions: {
-              position: window.google.maps.ControlPosition.RIGHT_CENTER
-          },
-          scaleControl: true,
-          streetViewControl: true,
-          streetViewControlOptions: {
-              position: window.google.maps.ControlPosition.RIGHT_BOTTOM
-          },
-          fullscreenControl: false
-    });
-
-    // Create an info window on the google map
-    let infowindow = new window.google.maps.InfoWindow();
-
-
-    this.state.venues.map(myVenue => {
-
-      let contentString = `${myVenue.venue.name}`;
-
-      // Create a marker on the google map
-      let marker = new window.google.maps.Marker({
-        position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
-        map: map,
-        title: myVenue.venue.name
-      });
-
-    marker.addListener('click', function() {
-
-      infowindow.setContent(contentString);
-
-      infowindow.open(map, marker);
-  });
-
-    })    
-  }
 
   render() {
     return (
       <main>
-        <div id="map"></div>
+        <MyMapComponent
+        {...this.props}
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDiucAl8uSDKELoUW25LTlq7xU7DsGG2hY"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100vh` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+/>
       </main>
     );
   }
 }
 
-let loadScript = (url) => {
-  let index = window.document.getElementsByTagName("script")[0]
-  let script = window.document.createElement("script")
-  script.src = url
-  script.async = true
-  script.defer = true
-  index.parentNode.insertBefore(script, index)
-}
 
 export default Map;
