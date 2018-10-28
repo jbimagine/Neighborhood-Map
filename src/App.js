39fbdfbb8a389db4f811d0b465443cf4e789ee44
Joe Bates <joebates@jbimagine.com>
2018-10-28 08:51:59 -0700

    EP-003 Update to use Vanilla Js for the map

--- src/App.js
+++ src/App.js
@@ -1,98 +1,124 @@
 import React, { Component } from 'react';
-import Navigation from './components/Navigation'
-import Map from './components/Map'
-import NeighborhoodApi from './components/NeighborhoodApi'
-import Sidebar from'./components/Sidemenu';
 import './App.css';
-
+import { load_google_maps, load_places, load_details } from './utils';
+import Navigation from './components/Navigation';
+import Sidemenu from './components/Sidemenu';
 
 class App extends Component {
   state = {
-    isMenuVisible: true,
-    venues:[],
-    markers:[],
-    center:[],
-    zoom:12,
-    updateSuperState: obj => {
-      this.setState(obj);
-    }
+    query:'',
+    isMenuVisible:true
   }
 
-  componentDidMount (){
-    document.body.classList.add('map-body')
+  componentDidMount() {
+    
+    let googleMapsPromise = load_google_maps();
+    let placesPromise = load_places();
 
-    NeighborhoodApi.search({
-      near: 'San Francisco, CA',
-      query: 'food'
-    }).then(results => {
-      const { venues } = results.response;
-      const { center } = results.response.geocode.feature.geometry;
-      const markers = venues.map(venue => {
-        return {
-          lat:venue.location.lat,
-          lng:venue.location.lng,
-          isOpen:false,
-          isVisible:true,
-          id: venue.id
-        }
+    Promise.all([
+      googleMapsPromise,
+      placesPromise,
+    ])
+    .then(values => {
+      let google = values[0];
+      this.venues = values[1].response.venues;
+      this.photos = values;
+      this.google = google;
+      this.markers = [];
+      this.infowindow = new google.maps.InfoWindow();
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        
+        // Set the initial settings for the google map
+        zoom:16,
+        scrollwheel:true,
+        center: { lat: this.venues[0].location.lat, lng: this.venues[0].location.lng },
+        mapTypeControl: true,	
+        mapTypeControlOptions: {	
+            style: window.google.maps.MapTypeControlStyle.HORIZONTAL_BAR,	
+            position: window.google.maps.ControlPosition.BOTTOM_CENTER	
+        },	
+        zoomControl: true,	
+        zoomControlOptions: {	
+            position: window.google.maps.ControlPosition.RIGHT_CENTER	
+        },	
+        scaleControl: true,	
+        streetViewControl: true,	
+        streetViewControlOptions: {	
+            position: window.google.maps.ControlPosition.RIGHT_BOTTOM	
+        },	
+        fullscreenControl: false
+      }); 
+
+      this.venues.forEach(venue => {
+        let marker = new google.maps.Marker({
+          position: { lat:venue.location.lat, lng: venue.location.lng },
+          map: this.map,
+          venue: venue,
+          id: venue.id,
+          name: venue.name,
+          animation: google.maps.Animation.DROP
+        });
+
+        // Handle the position of the map when marker is clicked
+        marker.addListener('click', () => {
+          if (marker.getAnimation() !== null) { marker.setAnimation(null); }
+				  else { marker.setAnimation(google.maps.Animation.BOUNCE); }
+				  setTimeout(() => { marker.setAnimation(null) }, 1500);
+			  });
+        google.maps.event.addListener(marker, 'click', () => {
+  			   this.infowindow.setContent(marker.name);
+				   this.map.setCenter(marker.position);
+				   this.infowindow.open(this.map, marker);
+				   this.map.panBy(0, -125);
+			  });
+
+        this.markers.push(marker);
       });
-      this.setState({ venues, center, markers })
+      this.setState({ filteredVenues:this.venues });
+    }).catch(()=> {
+      console.log('the venues are not available at this time, please try again later')
     })
-
   }
 
-  closeAllMarkers = () => {
-    const markers = this.state.markers.map(marker => {
-      marker.isOpen = false;
-      return marker;
-    });
-    this.setState( {markers: Object.assign(this.state.markers, markers)} )
+  filterVenues = (query) => {
+    let filter = this.venues.filter(venue => venue.name.toLowerCase().includes(query.toLowerCase()));
+
+    this.markers.forEach(marker => {
+      marker.name.toLowerCase().includes(query.toLowerCase())===true?
+      marker.setVisible(true):
+      marker.setVisible(false);
+    })
+     this.setState({ filteredVenues : filter, query })
   }
 
-  handleMarkerClick = marker => {
-    this.closeAllMarkers();
-    marker.isOpen = true;
-    this.setState( {markers: Object.assign(this.state.markers, marker)} )
-
-    const venue = this.state.venues.find(venue => venue.id === marker.id)
+  handleListItemClick = (venue) => {
+    let marker = this.markers.filter(marker => marker.id ===venue.id)[0];
+    this.infowindow.setContent(marker.name);
+    this.map.setCenter(marker.position);
+    this.infowindow.open(this.map, marker);
+    this.map.panBy(0, -125);
+    if (marker.getAnimation() !== null) { marker.setAnimation(null); }
+				  else { marker.setAnimation(this.google.maps.Animation.BOUNCE); }
+				  setTimeout(() => { marker.setAnimation(null) }, 1500);
+  }
   
-
-    NeighborhoodApi.getVenueDetails(marker.id).then(res => {
-      const newVenue = Object.assign(venue, res.response.venue);
-      this.setState({venues:Object.assign(this.state.venues, newVenue)})
-    })
-  };
-
-  handleListItemClick = venue => {
-    const marker = this.state.markers.find(marker => marker.id === venue.id)
-    this.handleMarkerClick(marker);
-  }
-
-  componentWillUnmount (){
-    document.body.classList.remove('map-body')
-  }
-
   handleMenuVisibility = () => {
     this.setState({isMenuVisible: !this.state.isMenuVisible})
   }
 
   render() {
     return (
-      <div>
-        <Sidebar 
-        {...this.state}
-      handleListItemClick ={this.handleListItemClick}
-      />
+      <div> 
       <Navigation
       isMenuVisible = {this.state.isMenuVisible}
       handleMenuVisibility = {this.handleMenuVisibility}
       />
-      <Map
+      <Sidemenu
       {...this.state}
-      handleMarkerClick = {this.handleMarkerClick}
+      handleListItemClick = {this.handleListItemClick}
       />
+      <div id="map"></div>
       </div>
-
     );
   }
 }
